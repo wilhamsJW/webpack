4f8fca28ac1e027e9270c6ca75c50d654a4fac1e
wilhamsJW <wilhams.dev@gmail.com>
2026-05-05 17:28:53 -0300

    feat adicionar PLUGIN

--- 3_PLUGINS/webpack.config.js
+++ 3_PLUGINS/webpack.config.js
@@ -1,51 +1,42 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+const DotenvPlugin = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
   mode: 'development',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          'style-loader', 'css-loader'
+          MiniCssExtractPlugin.loader, 'css-loader'
         ]
-      },
-      {
-        test: /\.(png|jpg|jpeg)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', 'css-loader', 'sass-loader'
-        ]
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread']
-          }
-        }
-      },
-      {
-        test: /\.html$/,
-        use: 'html-loader'
-      },
-      {
-        test: /\.txt$/,
-        use: 'raw-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify('1.0.2'),
+      PORT: JSON.stringify('8080')
+    }),
+    new DotenvPlugin(),
+    new HtmlWebpackPlugin()
+  ]
 }
