f718cb18e5fbd6a0a95fbdb8cd2abe27dcef50d5
wilhamsJW <wilhams.dev@gmail.com>
2026-05-05 17:12:12 -0300

    feat adicionar PLUGIN

---
+++ 3_PLUGINS/src/index.js
@@ -0,0 +1,9 @@
+import Heading from './components/heading/heading.js';
+
+const heading = new Heading();
+
+heading.create("Página principal");
+
+// console.log(VERSION);
+// console.log(PORT);
+// console.log(process.env.API_KEY);
